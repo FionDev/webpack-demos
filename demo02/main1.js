c91d5f8f64e4a6af9fae72ad34107ba0968ef2d7
Fion <fiondev7@gmail.com>
2015-09-06 11:05:15 +0800

    >載入main2.js using jquery

--- demo02/main1.js
+++ demo02/main1.js
@@ -1,8 +1,15 @@
+
+var $=require('jquery');
 document.write('<h1>Hello World</h1>');
 
+//way1---------------
 function load_main2(){
 	require('./main2.js');
 }
-
 window.load_main2=load_main2;
+//------------------
 
+$('button').on('click',function(){
+	require('./main2.js');
+
+});
